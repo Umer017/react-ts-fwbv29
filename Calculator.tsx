2f0ed7139c4e877abb165ea46ce80fa959485823
Umer017 <90950414+Umer017@users.noreply.github.com>
2022-09-12 12:40:56 +0530

    2 files modified

--- Calculator.tsx
+++ Calculator.tsx
@@ -5,13 +5,55 @@ export default function Calculator() {
   const [display, setDisplay] = React.useState({
     current: '0',
     total: '0',
+    isInitial: true,
+    preOp: '',
   });
   function NumberHandler(value) {
     let newVal = value;
-    newVal = display.current + value;
-    setDisplay({ current: newVal, total: display.total });
+    if (!display.isInitial) {
+      newVal = display.current + value;
+    }
+    setDisplay({
+      current: newVal,
+      total: display.total,
+      isInitial: false,
+      preOp: display.preOp,
+    });
   }
-  function OperaterHandler() {}
+  function OperaterHandler(value) {
+    var total = doCalculation();
+    setDisplay({
+      current: total.toString(),
+      total: total.toString(),
+      isInitial: true,
+      preOp: value,
+    });
+  }
+  function doCalculation() {
+    let total = parseInt(display.total);
+    switch (display.preOp) {
+      case '+':
+        total += parseInt(display.current);
+        break;
+      case '-':
+        total = total - parseInt(display.current);
+        break;
+      case '*':
+        total = total * parseInt(display.current);
+        break;
+      case '/':
+        total = total / parseInt(display.current);
+        break;
+      default:
+        total = parseInt(display.current);
+    }
+    return total;
+  }
+
+  function ClearHandler() {
+    setDisplay({ current: '0', total: '0', isInitial: true, preOp: '' });
+  }
+
   return (
     <div className="app-container">
       <div className="display">{display.current}</div>
@@ -30,9 +72,9 @@ export default function Calculator() {
       <Calbutton value="3" onClick={NumberHandler} />
       <Calbutton className="operator" value="-" onClick={OperaterHandler} />
 
-      <Calbutton value="C" />
+      <Calbutton value="C" onClick={ClearHandler} />
       <Calbutton value="0" onClick={NumberHandler} />
-      <Calbutton value="=" />
+      <Calbutton value="=" onClick={OperaterHandler} />
       <Calbutton className="operator" value="+" onClick={OperaterHandler} />
     </div>
   );
